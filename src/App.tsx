ecab2ecd2f4da526774a58afd5b90a504f112bff
Obdurat <omicron_rpl@hotmail.com>
2023-03-16 19:25:25 -0300

    feat: Added full calendar package

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {useDispatch} from 'react-redux';
 import {Header} from './Components/Header';
 import Dropdown from './Components/Dropdown';
-import Invoices from './Pages/Invoices';
 import {useSelector} from 'react-redux';
 import type {RootState} from '@Store/ReduxStore';
 import {toogle} from '@States/dropDown';
